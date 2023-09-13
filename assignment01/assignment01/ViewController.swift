d168a40ed6d945e724993e783198223db8aecf3c
sanc93 <sanc93@me.com>
2023-09-13 14:28:47 +0900

    chore: 프로젝트 생성

---
+++ assignment01/assignment01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment01
+//
+//  Created by Sanghun K. on 9/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
