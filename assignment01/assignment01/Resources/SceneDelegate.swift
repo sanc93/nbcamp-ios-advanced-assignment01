cd7598db7319dd39930529fcb98f5a0b5d639793
sanc93 <sanc93@me.com>
2023-09-14 15:37:03 +0900

    chore: Storyboard없이 코드 베이스로 작업 위한 준비, 기본 폴더링

--- assignment01/assignment01/Resources/SceneDelegate.swift
+++ assignment01/assignment01/Resources/SceneDelegate.swift
@@ -16,7 +16,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        // 메인 뷰 컨트롤러를 navigation controller로 감싸기
+        let entryVC = EntryViewController()
+        let navigationController = UINavigationController(rootViewController: entryVC)
+
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
